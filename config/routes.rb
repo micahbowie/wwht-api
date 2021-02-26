23a1a2574a5d2d7dfc08ac3e969d364e72147765
Micah Bowie <micahbowie20@gmail.com>
2021-02-26 10:30:11 -0500

    made new emailverification controller. using api to validate users email

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,7 @@ Rails.application.routes.draw do
 
   # Joke
   get "/joke", to: 'joke_api#show'
+
+  # Email verify
+  get "/verify", to: 'everify#verify'
 end
---
+++ test/controllers/everify_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EverifyControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
