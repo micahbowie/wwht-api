32e4de3e4f4586841f46d9c851cc74773c077c77
Micah Bowie <micahbowie20@gmail.com>
2021-03-09 22:45:03 -0500

    added has secure password and bcrypt. added password column to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :lists
-  
+
   validates :name, presence: true, length: { maximum: 25 }
   validates :email, presence: true, length: { maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_151120) do
+ActiveRecord::Schema.define(version: 2021_03_10_033502) do
 
   create_table "list_terms", force: :cascade do |t|
     t.integer "list_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_151120) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
