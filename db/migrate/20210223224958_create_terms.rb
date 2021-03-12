79fca565368df7d1413b429aef0e196392f96755
Micah Bowie <micahbowie20@gmail.com>
2021-03-11 19:46:16 -0500

    changed term to only have name column.

--- db/migrate/20210223224958_create_terms.rb
+++ db/migrate/20210223224958_create_terms.rb
@@ -2,9 +2,6 @@ class CreateTerms < ActiveRecord::Migration[6.0]
   def change
     create_table :terms do |t|
       t.string :name
-      t.text :why
-      t.text :what
-      t.text :how
       t.timestamps
     end
   end
