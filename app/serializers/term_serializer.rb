b85d0ff41356c0fb6017157461817937851debee
Micah Bowie <micahbowie20@gmail.com>
2021-03-11 19:44:34 -0500

    removed why what and how. added association to definition

--- app/serializers/term_serializer.rb
+++ app/serializers/term_serializer.rb
@@ -1,6 +1,7 @@
 class TermSerializer < ActiveModel::Serializer
-  attributes :id, :name, :why, :what, :how
+  attributes :id, :name
 
+  has_many :definitions
   has_many:list_terms
   has_many :lists, through: :list_terms
 end
