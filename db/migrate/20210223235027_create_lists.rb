ec1540c5bba52dd8ee0f936b896b1e97b556edca
Micah Bowie <micahbowie20@gmail.com>
2021-03-19 13:23:18 -0400

    changed datatbase from sqlite to postgresql. updated t.string to t.text

--- db/migrate/20210223235027_create_lists.rb
+++ db/migrate/20210223235027_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration[6.0]
   def change
     create_table :lists do |t|
-      t.string :name
+      t.text :name
       t.integer :user_id
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_11_234552) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "definitions", force: :cascade do |t|
     t.text "what"
     t.text "why"
