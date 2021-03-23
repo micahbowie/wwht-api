b7a5ade2c064a0ae125dc5381e2eff7b9b1299f5
Micah Bowie <micahbowie20@gmail.com>
2021-03-23 10:22:29 -0400

    added a description colum to list.

--- db/migrate/20210223235027_create_lists.rb
+++ db/migrate/20210223235027_create_lists.rb
@@ -2,6 +2,7 @@ class CreateLists < ActiveRecord::Migration[6.0]
   def change
     create_table :lists do |t|
       t.text :name
+      t.text :description
       t.integer :user_id
       t.timestamps
     end
