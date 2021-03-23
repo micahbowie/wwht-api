e1fec1b2dc71f31dfbbfae6ae3d08883e0087e2d
Micah Bowie <micahbowie20@gmail.com>
2021-03-23 10:17:09 -0400

    created auth controller and added a login method for jwt auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::AuthController < ApplicationController
+  skip_before_action :authorized, only: [:login]
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      render json: { user: @user jwt: token }, status: :accepted
+    else
+      render json: {errors:"Invalid email or password"}, status: :unauthorized
+    end
+  end
+
 end
