faf16edf40b6d1cc7fe3f198d32b7657655b3086
Micah Bowie <micahbowie20@gmail.com>
2021-03-23 11:10:36 -0400

    updated serializers and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :lists
 
-  validates :name, presence: true, length: { maximum: 25 }
+  validates :first_name, presence: true, length: { maximum: 25 }
+  validates :last_name, presence: true, length: { maximum: 25 }
   validates :email, presence: true, length: { maximum: 50 }
 
   default_scope { order('id ASC') }
