bc7f89a47f7b609235f1e8eca259f388909120f0
Micah Bowie <micahbowie20@gmail.com>
2021-03-29 23:57:23 -0400

    added oneliner to defintion serializer

--- app/serializers/definition_serializer.rb
+++ app/serializers/definition_serializer.rb
@@ -1,5 +1,5 @@
 class DefinitionSerializer < ActiveModel::Serializer
-  attributes :id, :what, :why, :how, :up_vote, :one_liner
+  attributes :id, :what, :why, :how, :up_vote, :one_liner, :images
 
   has_many :list_definitions
   has_many :lists, through: :list_definitions
